c079277ef1890b16b67c0c999ccbef4685c9256b
ozor <ozor@ngs.ru>
2019-12-11 03:13:12 +0700

    Add BS algorithm skeleton

---
+++ algorithms/Binary-Search/README.md
@@ -0,0 +1,7 @@
+# Binary Search
+
+Binary Search describes the process of searching for a specific value in an ordered collection.
+
+[Wikipedia definition][1].
+
+[1]: https://en.wikipedia.org/wiki/Binary_search_algorithm
---
+++ algorithms/Binary-Search/Solution.php
@@ -0,0 +1,20 @@
+<?php
+
+class Solution
+{
+    /**
+     * Given a sorted (in ascending order) integer array nums of n elements and a target value.
+     * This function searches target in nums.
+     * If target exists, then return its index, otherwise return -1.
+     *
+     * @param int[] $nums
+     * @param int $target
+     * @return int
+     */
+    function search(array $nums, int $target): int
+    {
+        // TODO: Implement Binary Search algorithm
+
+        return 0;
+    }
+}
